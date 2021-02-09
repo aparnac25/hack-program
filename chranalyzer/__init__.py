ba0db699d4b6a410aa8169f1073dbddfffae0f88
aparnac25 <aparnac25@gmail.com>
2021-02-09 18:45:35 -0500

    Folder with chranlzyer program but trouble with install

---
+++ chranalyzer/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""
+chranalyze program for analzying the number of characters when inputted into the command line. Will spit out table and plot. 
+"""
+
+__version__ = "0.0.1"
+
+import chranalyzer
---
+++ chranalyzer/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+Command line interface to run chranalyzer
+"""
+
+import chranalyzer
+
+if __name__ == '__main__':
+    chranalyze(input_data)
+
+    
