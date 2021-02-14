e72bda0251202f22fca60759906e3dd4e4e564ae
aparnac25 <61798929+aparnac25@users.noreply.github.com>
2021-02-14 16:47:45 -0500

    Delete chranalyzer directory

--- chranalyzer/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-"""
-chranalyze program for analzying the number of characters when inputted into the command line. Will spit out table and plot. 
-"""
-
-__version__ = "0.0.1"
-
-import chranalyzer
